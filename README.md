190bb5171806bc1ebdea8beb4f6647b77389cbeb
Jun Ni <jun.ni@gmail.com>
2021-01-30 19:32:03 +0800

    db-starter-kit-1.x-dev

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## MySQL
+---
+示例数据库 employees.sql sakila.sql world.sql world_x.sql 都可以直接导入 MySQL。
--- note.sql
+++ note.sql
@@ -1,6 +1,11 @@
 -- 服务器状态确认
 show plugins;
 
+SELECT table_schema "DB Name",
+        ROUND(SUM(data_length + index_length) / 1024 / 1024, 1) "DB Size in MB" 
+FROM information_schema.tables 
+GROUP BY table_schema; -- 确认 DB 大小
+
 -- 常用语句
 CREATE DATABASE DB_NAME DEFAULT charset utf8mb4 COLLATE utf8mb4_general_ci;
 GRANT ALL PRIVILEGES ON *.* TO `USER`@'%' IDENTIFIED BY 'PASSWORD';
@@ -9,6 +14,7 @@ SHOW databases;
 
 
 -- 示例数据库 employees
+
 USE employees;
 DESC salaries;
 select count(*) from salaries;
